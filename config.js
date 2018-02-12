a2847919ea6e57cb8ce32a3456c78e6187c41283
Muhammad Osama Arshad <muhammadosamaarshad@gmail.com>
2018-02-13 00:12:07 +0500

    Fix: modify apiUrl in config

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
 const config =  {
-    apiUrl: 'http://localhost:3001',
-    // apiUrl: 'http://jobi-test-server.herokuapp.com',
+    // apiUrl: 'http://localhost:3001',
+    apiUrl: 'http://jobi-test-server.herokuapp.com',
 }
 
 export default config;
